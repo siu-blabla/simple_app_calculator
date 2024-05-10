0dc4ebc24fd7b6080721e8663850f70e18043e82
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-05-10 18:55:14 +0800

    Added pseudocode

---
+++ simple_app_calculator_exception_handling.py
@@ -0,0 +1,7 @@
+# Introduction
+# Display Options
+# Ask the user to choose their desired math operation
+# Ask user to enter the first number
+# Ask user to enter the second number
+# Execute the chosen math operation and display the output
+# Looping (Ask user if they want to try again)
